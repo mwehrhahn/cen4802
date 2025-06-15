00a6f1f36d1ae73178ef3c52b8278a7461eac9d8
mwehrhahn <81428787+mwehrhahn@users.noreply.github.com>
2025-06-15 13:42:16 -0400

    removed unused import

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -1,15 +1,9 @@
 package com.mycompany.app;
 
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int num1;
-        System.out.println("Enter a number to get its Fibonacci number");
-        Scanner sc = new Scanner(System.in);
-        num1 = sc.nextInt();
-        Fibonacci fibonacci = new Fibonacci();
-        System.out.print(fibonacci.fibonacci(num1));
+        System.out.println("Fibonacci(10) = " + new Fibonacci().fibonacci(10));
     }
 }
