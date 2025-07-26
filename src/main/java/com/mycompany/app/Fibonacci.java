40e0ec54c36798ce400cf1ab3c12e3ab0e8734f2
mwehrhahn <81428787+mwehrhahn@users.noreply.github.com>
2025-07-26 13:17:33 -0400

    Added logging and introduced a REST API

--- src/main/java/com/mycompany/app/Fibonacci.java
+++ src/main/java/com/mycompany/app/Fibonacci.java
@@ -1,16 +1,19 @@
 package com.mycompany.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Fibonacci {
     /**
      * Method that calculates and returns the nth term of the Fibonacci sequence using recursion
      * @param n, the position of the term to calculate, 1-based index
      * @return the nth fibonacci number
      */
+
+    private static final Logger logger = LoggerFactory.getLogger(Fibonacci.class);
     public static int fibonacci(int n)
     {
-        if (n < 0) {
-            throw new IllegalArgumentException("Input must be non-negative");
-        }
+        logger.info("Fibonacci(n) = {}", n);
         if (n <= 1)
         {
             return n;
--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -1,9 +1,24 @@
 package com.mycompany.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
 
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Fibonacci(10) = " + new Fibonacci().fibonacci(10));
+        Scanner sc = new Scanner(System.in);
+        int input = 0;
+
+        System.out.print("Enter the number of fibonacci numbers: ");
+        input = sc.nextInt();
+
+        long result = Fibonacci.fibonacci(input);
+        logger.info("Result: {}", result);
+
+        System.out.println("Fibonacci of " + input + " is " + result);
     }
 }
